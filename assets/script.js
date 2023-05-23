5c41a22eb5d1d7edacbcd6c3337f4559e18da85e
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-05-23 13:06:40 -0400

    Added date and made sure code working accoring to specs

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,12 @@
 var APIKey = "4c40bc90efb07222ff1d873fc634eb67";
 
+const searchForm = document.querySelector("#searchForm");
+
+searchForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+  currentCity();
+});
+
 // Move the queryURL inside the forecastCity function
 function currentCity () {
     const city = document.querySelector("#citySrch").value;
@@ -77,14 +84,17 @@ searchBtn.addEventListener("click", currentCity);
 
 // Load Current Weather
 const currentLoc = document.querySelector("#currentLoc");
+const currentDateElement = document.querySelector("#currentDate");
 const currentSymb = document.querySelector("#currentSymb");
 const currentTemp = document.querySelector("#currentTemp");
 const currentWind = document.querySelector("#currentWind");
 const currentHum = document.querySelector("#currentHum");
+const currentDate = new Date().toLocaleString('en-US', { weekday: 'long', hour: 'numeric', minute: 'numeric', hour12: true });
 
 function loadWeather(pulledData) {
     console.log(pulledData);
     currentLoc.innerHTML = pulledData.name;
+    currentDateElement.innerHTML = currentDate;
     currentSymb.src = "http://openweathermap.org/img/w/" + pulledData.weather[0].icon + ".png";
     currentSymb.alt = pulledData.weather[0].description;
     currentTemp.innerHTML = "Temp: " + pulledData.main.temp + "°C";
@@ -93,11 +103,9 @@ function loadWeather(pulledData) {
 }
 
 // Load Forecast Weather
-
 const forecast = document.querySelector("#forecast");
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// const days5 = [6,14,22,30,38];
 function loadForecast(pulledData) {
     console.log(pulledData);
 
@@ -107,17 +115,17 @@ function loadForecast(pulledData) {
     }
 
     // Array to store the reference points for midday of the next 5 days
+    // Required as API is limited to forecast every 3 hours, not daily. Assumption made that midday sufficient
     const days5 = []; 
     var currentDate = new Date();
     currentDate.setHours(12, 0, 0, 0);
-    console.log(currentDate);
+
     // Iterate through the forecast data and find the next 5 midday reference points
     for (let i = 1; i <= 5; i++) {
         // Calculate the target date by adding 'i' days to the current date
         var targetDate = new Date(currentDate);
         targetDate.setDate(targetDate.getDate() + i);
         targetDate.setHours(12, 0, 0, 0);
-        console.log(targetDate);
 
         // Find the index of the forecast data that matches the target date and time
         var targetIndex = pulledData.list.findIndex(item => {
@@ -130,13 +138,15 @@ function loadForecast(pulledData) {
             days5.push(targetIndex);
         }
     }
-    console.log(days5);
 
+    // Iterate through each of the midday points to populate dayCards
     for (let i =0; i < 5; i++) {
         var dayCard = document.createElement("div");
         dayCard.className = "dayCard";
 
         var dayCardName = document.createElement("p");
+        dayCardName.className = "dayName"
+        var dayCardDate = document.createElement("p");
         var dayCardSymb = document.createElement("img");
         var dayCardTemp = document.createElement("p");
         var dayCardWind = document.createElement("p");
@@ -144,18 +154,23 @@ function loadForecast(pulledData) {
 
         forecast.appendChild(dayCard);
         dayCard.appendChild(dayCardName);
+        dayCard.appendChild(dayCardDate);
         dayCard.appendChild(dayCardSymb);
         dayCard.appendChild(dayCardTemp);
         dayCard.appendChild(dayCardWind);
         dayCard.appendChild(dayCardHum);
 
-        // Parse the date from the response
+            // Parse the date from the response
         var date = new Date(pulledData.list[days5[i]].dt_txt);
         // Get the day of the week
         var dayOfWeek = days[date.getDay()];
+        // Get the date in the format "MM/DD"
+        var formattedDate = (date.getMonth() + 1) + "/" + date.getDate();
 
+        dayCardDate.innerHTML = formattedDate; // Set the date content
         dayCardName.innerHTML = dayOfWeek;
         dayCardSymb.src = "http://openweathermap.org/img/wn/" + pulledData.list[days5[i]].weather[0].icon + ".png";
+        dayCardSymb.alt = pulledData.list[days5[i]].weather[0].description;
         dayCardTemp.innerHTML = "Temp: " + pulledData.list[days5[i]].main.temp + "°C";
         dayCardWind.innerHTML = "Wind: " + pulledData.list[days5[i]].wind.speed + "m/s";
         dayCardHum.innerHTML = "Humidity: " + pulledData.list[days5[i]].main.humidity+ "%";
