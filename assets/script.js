2eb593f62db7dcf47009214f4bcf4bb2c4291eb4
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-05-23 12:43:55 -0400

    Finalised most of JS - added weather cards and funcationality to create forecast

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
-var APIKey = "ce2a80e4badc9c86136b1d15faae58b9";
+var APIKey = "4c40bc90efb07222ff1d873fc634eb67";
 
-// Move the queryURL inside the updateCity function
-function updateCity() {
-    const city = document.querySelector("#citySrch").value; // Use .value to get the input value
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-  
-  console.log(city);
+// Move the queryURL inside the forecastCity function
+function currentCity () {
+    const city = document.querySelector("#citySrch").value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=metric";
+
   fetch(queryURL)
     .then(function(response) {
       return response.json();
     })
     .then(function(jsonData) {
       const pulledData = jsonData;
-      console.log(pulledData);
+      loadWeather(pulledData);
+    });
+
+    forecastCity();
+}
+
+
+function forecastCity() {
+    const city = document.querySelector("#citySrch").value;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=metric";
+  
+  fetch(queryURL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonData) {
+      const pulledData = jsonData;
+      loadForecast(pulledData);
     });
 
     logCity(city);
 }
-
 
 // LOCAL STORAGE
 let cityList = JSON.parse(localStorage.getItem("cityList")) || [];
@@ -27,7 +42,7 @@ function logCity(city){
     // Check if the city already exists in the cityList
     const existingIndex = cityList.indexOf(city);
     if (existingIndex !== -1) {
-        // If the city exists, remove it from its current position
+    // If the city exists, remove it from its current position
         cityList.splice(existingIndex, 1);
     }
     
@@ -53,12 +68,96 @@ function logCity(city){
 
 function reload(cityOld) {
     document.querySelector("#citySrch").value = cityOld;
-    updateCity()
+    currentCity()
 }
 
-
-
-
-
 const searchBtn = document.querySelector("#searchBtn");
-searchBtn.addEventListener("click", updateCity);
+searchBtn.addEventListener("click", currentCity);
+
+
+// Load Current Weather
+const currentLoc = document.querySelector("#currentLoc");
+const currentSymb = document.querySelector("#currentSymb");
+const currentTemp = document.querySelector("#currentTemp");
+const currentWind = document.querySelector("#currentWind");
+const currentHum = document.querySelector("#currentHum");
+
+function loadWeather(pulledData) {
+    console.log(pulledData);
+    currentLoc.innerHTML = pulledData.name;
+    currentSymb.src = "http://openweathermap.org/img/w/" + pulledData.weather[0].icon + ".png";
+    currentSymb.alt = pulledData.weather[0].description;
+    currentTemp.innerHTML = "Temp: " + pulledData.main.temp + "°C";
+    currentWind.innerHTML = "Wind: " + pulledData.wind.speed + "m/s";
+    currentHum.innerHTML = "Humidity: " + pulledData.main.humidity + "%";
+}
+
+// Load Forecast Weather
+
+const forecast = document.querySelector("#forecast");
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// const days5 = [6,14,22,30,38];
+function loadForecast(pulledData) {
+    console.log(pulledData);
+
+    // Remove existing forecast
+    while (forecast.firstChild) {
+        forecast.removeChild(forecast.firstChild);
+    }
+
+    // Array to store the reference points for midday of the next 5 days
+    const days5 = []; 
+    var currentDate = new Date();
+    currentDate.setHours(12, 0, 0, 0);
+    console.log(currentDate);
+    // Iterate through the forecast data and find the next 5 midday reference points
+    for (let i = 1; i <= 5; i++) {
+        // Calculate the target date by adding 'i' days to the current date
+        var targetDate = new Date(currentDate);
+        targetDate.setDate(targetDate.getDate() + i);
+        targetDate.setHours(12, 0, 0, 0);
+        console.log(targetDate);
+
+        // Find the index of the forecast data that matches the target date and time
+        var targetIndex = pulledData.list.findIndex(item => {
+            var itemDate = new Date(item.dt_txt);
+            return itemDate.getTime() === targetDate.getTime();
+        });
+
+        // If the index is found, add it to the days5 array
+        if (targetIndex !== -1) {
+            days5.push(targetIndex);
+        }
+    }
+    console.log(days5);
+
+    for (let i =0; i < 5; i++) {
+        var dayCard = document.createElement("div");
+        dayCard.className = "dayCard";
+
+        var dayCardName = document.createElement("p");
+        var dayCardSymb = document.createElement("img");
+        var dayCardTemp = document.createElement("p");
+        var dayCardWind = document.createElement("p");
+        var dayCardHum = document.createElement("p");
+
+        forecast.appendChild(dayCard);
+        dayCard.appendChild(dayCardName);
+        dayCard.appendChild(dayCardSymb);
+        dayCard.appendChild(dayCardTemp);
+        dayCard.appendChild(dayCardWind);
+        dayCard.appendChild(dayCardHum);
+
+        // Parse the date from the response
+        var date = new Date(pulledData.list[days5[i]].dt_txt);
+        // Get the day of the week
+        var dayOfWeek = days[date.getDay()];
+
+        dayCardName.innerHTML = dayOfWeek;
+        dayCardSymb.src = "http://openweathermap.org/img/wn/" + pulledData.list[days5[i]].weather[0].icon + ".png";
+        dayCardTemp.innerHTML = "Temp: " + pulledData.list[days5[i]].main.temp + "°C";
+        dayCardWind.innerHTML = "Wind: " + pulledData.list[days5[i]].wind.speed + "m/s";
+        dayCardHum.innerHTML = "Humidity: " + pulledData.list[days5[i]].main.humidity+ "%";
+    }
+}
